552beb57ae7f213340f4af135859ae522a14a8e0
milad-sol <lvlilad_soltani@yahoo.com>
2024-11-30 08:56:12 +0330

    Create Master theme create essential apps

--- shop/settings.py
+++ shop/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+    'home.apps.HomeConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
