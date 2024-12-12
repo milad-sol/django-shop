82fe82baa8603f6c85cd2feb92ced872f773587b
milad-sol <lvlilad_soltani@yahoo.com>
2024-12-12 10:33:00 +0330

    edit managment commands

--- accounts/management/commands/remove_expired_otp.py
+++ accounts/management/commands/remove_expired_otp.py
@@ -1,12 +1,12 @@
 from django.core.management.base import BaseCommand
 from accounts.models import OtpCode
 from datetime import datetime, timedelta
-
+import pytz
 
 class Command(BaseCommand):
     help = 'Remove expired OTP codes'
 
     def handle(self, *args, **options):
-        expired_time = datetime.now() - timedelta(minutes=2)
+        expired_time = datetime.now(tz=pytz.timezone('Asia/Tehran')) - timedelta(minutes=2)
         OtpCode.objects.filter(created__lt=expired_time).delete()
         self.stdout.write(self.style.SUCCESS('OTP code removed'))
